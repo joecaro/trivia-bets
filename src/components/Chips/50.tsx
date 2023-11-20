e8ce6364d9a83c943efbd6d34d65e0c81473481b
Joseph Carothers <jsc11598@gmail.com>
2023-11-19 21:51:05 -0500

    fix svg props

--- src/components/Chips/50.tsx
+++ src/components/Chips/50.tsx
@@ -1,23 +1,23 @@
 export default function FiftySvg() {
     return (
-        <svg height="100%" stroke-miterlimit="10" style={{ fillRule: 'nonzero', clipRule: 'evenodd', strokeLinecap: 'round', strokeLinejoin: 'round' }} version="1.1" viewBox="0 0 110 110" width="100%" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        <svg height="100%" strokeMiterlimit="10" style={{ fillRule: 'nonzero', clipRule: 'evenodd', strokeLinecap: 'round', strokeLinejoin: 'round' }} version="1.1" viewBox="0 0 110 110" width="100%" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
             <clipPath id="ArtboardFrame">
                 <rect height="110" width="110" x="0" y="0" />
             </clipPath>
-            <g clip-path="url(#ArtboardFrame)" id="Layer-1">
+            <g clipPath="url(#ArtboardFrame)" id="Layer-1">
                 <use opacity="1" transform="matrix(0.61711 0 0 0.61711 -189.208 129.07)" xlinkHref="#Image" />
             </g>
             <g id="Layer-2">
-                <path d="M5.10876 53.3087C5.10876 25.322 27.7964 2.63439 55.7831 2.63439C83.7697 2.63439 106.457 25.322 106.457 53.3087C106.457 81.2953 83.7697 103.983 55.7831 103.983C27.7964 103.983 5.10876 81.2953 5.10876 53.3087Z" fill="#3a0c60" fill-rule="nonzero" opacity="1" stroke="none" />
-                <path d="M23.0228 53.3087C23.0228 35.2157 37.6901 20.5484 55.7831 20.5484C73.876 20.5484 88.5433 35.2157 88.5433 53.3087C88.5433 71.4017 73.876 86.0689 55.7831 86.0689C37.6901 86.0689 23.0228 71.4017 23.0228 53.3087Z" fill="none" opacity="1" stroke="#ffffff" stroke-linecap="butt" stroke-linejoin="round" stroke-width="2.3824" />
+                <path d="M5.10876 53.3087C5.10876 25.322 27.7964 2.63439 55.7831 2.63439C83.7697 2.63439 106.457 25.322 106.457 53.3087C106.457 81.2953 83.7697 103.983 55.7831 103.983C27.7964 103.983 5.10876 81.2953 5.10876 53.3087Z" fill="#3a0c60" fillRule="nonzero" opacity="1" stroke="none" />
+                <path d="M23.0228 53.3087C23.0228 35.2157 37.6901 20.5484 55.7831 20.5484C73.876 20.5484 88.5433 35.2157 88.5433 53.3087C88.5433 71.4017 73.876 86.0689 55.7831 86.0689C37.6901 86.0689 23.0228 71.4017 23.0228 53.3087Z" fill="none" opacity="1" stroke="#ffffff" strokeLinecap="butt" strokeLinejoin="round" strokeWidth="2.3824" />
             </g>
             <g id="Layer-3">
-                <path d="M18.4688 19.25C13.4483 24.7472 9.71969 31.3502 7.5 38.6562L17.0625 44.0938L28.0938 24.7188L18.4688 19.25Z" fill="#ffffff" fill-rule="nonzero" opacity="1" stroke="none" />
-                <path d="M94.5 62.5312L86.4375 76.6875L83.4688 81.9062L93.0938 87.375C98.1142 81.8778 101.843 75.2748 104.062 67.9688L94.5 62.5312Z" fill="#ffffff" fill-rule="nonzero" opacity="1" stroke="none" />
-                <path d="M17.0625 62.5312L7.53125 68C9.75701 75.3056 13.4735 81.912 18.5 87.4062L28.125 81.9062L17.0625 62.5312Z" fill="#ffffff" fill-rule="nonzero" opacity="1" stroke="none" />
-                <path d="M93.0625 19.2188L83.4375 24.6875L91.5312 38.8438L94.5 44.0625L104.031 38.625C101.805 31.3194 98.089 24.713 93.0625 19.2188Z" fill="#ffffff" fill-rule="nonzero" opacity="1" stroke="none" />
-                <path d="M44.625 91.5L44.625 102.688C48.2226 103.498 51.9389 103.969 55.7812 103.969C59.6236 103.969 63.3399 103.498 66.9375 102.688L66.9375 91.5L44.625 91.5Z" fill="#ffffff" fill-rule="nonzero" opacity="1" stroke="none" />
-                <path d="M55.7812 2.625C51.9389 2.625 48.2226 3.12722 44.625 3.9375L44.625 15.0938L60.9375 15.0938L66.9375 15.0938L66.9375 3.9375C63.3399 3.12722 59.6236 2.625 55.7812 2.625Z" fill="#ffffff" fill-rule="nonzero" opacity="1" stroke="none" />
+                <path d="M18.4688 19.25C13.4483 24.7472 9.71969 31.3502 7.5 38.6562L17.0625 44.0938L28.0938 24.7188L18.4688 19.25Z" fill="#ffffff" fillRule="nonzero" opacity="1" stroke="none" />
+                <path d="M94.5 62.5312L86.4375 76.6875L83.4688 81.9062L93.0938 87.375C98.1142 81.8778 101.843 75.2748 104.062 67.9688L94.5 62.5312Z" fill="#ffffff" fillRule="nonzero" opacity="1" stroke="none" />
+                <path d="M17.0625 62.5312L7.53125 68C9.75701 75.3056 13.4735 81.912 18.5 87.4062L28.125 81.9062L17.0625 62.5312Z" fill="#ffffff" fillRule="nonzero" opacity="1" stroke="none" />
+                <path d="M93.0625 19.2188L83.4375 24.6875L91.5312 38.8438L94.5 44.0625L104.031 38.625C101.805 31.3194 98.089 24.713 93.0625 19.2188Z" fill="#ffffff" fillRule="nonzero" opacity="1" stroke="none" />
+                <path d="M44.625 91.5L44.625 102.688C48.2226 103.498 51.9389 103.969 55.7812 103.969C59.6236 103.969 63.3399 103.498 66.9375 102.688L66.9375 91.5L44.625 91.5Z" fill="#ffffff" fillRule="nonzero" opacity="1" stroke="none" />
+                <path d="M55.7812 2.625C51.9389 2.625 48.2226 3.12722 44.625 3.9375L44.625 15.0938L60.9375 15.0938L66.9375 15.0938L66.9375 3.9375C63.3399 3.12722 59.6236 2.625 55.7812 2.625Z" fill="#ffffff" fillRule="nonzero" opacity="1" stroke="none" />
             </g>
         </svg>
     )
